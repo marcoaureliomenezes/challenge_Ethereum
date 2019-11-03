184e94c7e6153bf01e77752b1e43b41bbd920927
Marco Aurelio Reis Lima Menezes <42839553+marcoaureliomenezes@users.noreply.github.com>
2019-11-03 02:25:08 +0100

    Final version.

--- src/MathExpression.cpp
+++ src/MathExpression.cpp
@@ -2,33 +2,30 @@
 #include <vector>
 #include <stack>
 #include <iostream>
-#include "windows.h"
 std::string op = "+-*/";
 
-// Public method where the private method are called.
-int MathExpression::readLine(std::string input) {
+int MathExpression::readLine(std::string input)  {
 	std::vector<char> v1;
-	// First, the string is formatted.
 	v1 = MathExpression::formatInput(input);
-	// Second, it is tested if the entry is valid.
 	bool check = MathExpression::checkLine(v1);
-	// If not, the method returns -1.
-	if (check == false) { return(-1); }
-	// Otherwise, it returns the result of the expression.
-	else { return (MathExpression::solveExpression(v1)); }
+	if (check == false) { 
+		return(-1); 
+	}
+	else { 
+		return (MathExpression::solveExpression(v1)); 
+	}
 }
-int MathExpression::operation(int a, int b, char c) 
-{
+int MathExpression::operation(int a, int b, char c)  {
 	switch (c) {
 	case '+': return a + b;
 	case '-': return a - b;
 	case '*': return a * b;
 	case '/':
 		try {
-			if (b == 0) { throw 10; }
+			if (b == 0) { throw 0; }
 		}
 		catch (int x) {
-			std::cout << "Error " << x << "! Division by 0." << std::endl;
+			std::cout << "Error " << x << "  - Division by 0." << std::endl;
 			return -1;
 		}
 		return a / b;
@@ -37,17 +34,15 @@ int MathExpression::operation(int a, int b, char c)
 		return -1;
 	}
 }
-std::vector<char> MathExpression::formatInput(std::string input)
-{	
-	// First spaces are eliminated from the string.
+std::vector<char> MathExpression::formatInput(std::string input)  {	
+	// Eliminates spaces from the string.
 	std::vector<char> v1; std::vector<char> v2;
 	for (int i = 0; i < input.size(); i++) {
 		if (input[i] != ' ') {
 			v2.push_back(input[i]);
 		}
 	}
-	// Second, consecultive operator of '+' and '-' are simplified.
-	// For example, the sequence '+-+----+++-' is converted in '+'.
+	// Simplifies consecultive operators of '+' and '-'.
 	int i = 0, k = v2.size() - 1;
 	while (v2[k] == '+' || v2[k] == '-') {
 		k--;
@@ -71,8 +66,7 @@ std::vector<char> MathExpression::formatInput(std::string input)
 		else { v2.push_back(v1[i]); i++; }
 	}
 	v1.clear();
-	// Lastly, the useless operators of '+' are eliminated.
-	// For example '+5+(+2)' is converted in '5+(2)'
+	// Removes useless operators of '+'.
 	for (int i = 0; i < v2.size(); i++) {
 		if (v2[i] == '+') {
 			if ((i == 0 || i == v2.size())) {}
@@ -84,10 +78,7 @@ std::vector<char> MathExpression::formatInput(std::string input)
 	}
 	return v1;
 }
-// The solveOperation method makes a simple operation using the stack of numbers and
-// stack of operators. In the end stack the result in the stack of numbers.
-void MathExpression::solveOperation(std::stack<int>& numStack, std::stack<char>& opStack)
-{
+void MathExpression::solveOperation(std::stack<int>& numStack, std::stack<char>& opStack)  {
 	int val1, val2, result; char op;
 	val1 = numStack.top();	numStack.pop();
 	op = opStack.top();		opStack.pop();
@@ -95,68 +86,48 @@ void MathExpression::solveOperation(std::stack<int>& numStack, std::stack<char>&
 	result = MathExpression::operation(val2, val1, op);
 	numStack.push(result);
 }
-// The solveExpression method solves an expression passed by a vector
-// and returns an interger.
-int MathExpression::solveExpression(std::vector<char> v1)
-{
+int MathExpression::solveExpression(std::vector<char> v1)  {
 	std::stack<int> numStack; std::stack<char> opStack;
 	std::stack<int>* ptNumStack; std::stack<char>* ptOpStack;
 	ptNumStack = &numStack; ptOpStack = &opStack;
-	// while it runs through the vector elements:
 	for (int i = 0; i < v1.size(); i++) {
 		for (int j = 0; j < op.size(); j++) {
-			// If it's an operator this operator is stacked on
-			// opStack
 			if (v1[i] == op[j]) {
 				opStack.push(v1[i]);
 				break;
 			}
 		}
-		// If it's a parenthesis being opened this is stacked
-		// on opStack too.
 		if (v1[i] == '(') { opStack.push(v1[i]); }
-		// If it's a literal, it's converted to an integer
 		if (isdigit(v1[i])) {
 			int number = int(v1[i]) - int('0');
 			numStack.push(number);
-			// If the last operator is '*' or '/' the operation is solved.
 			if ((opStack.size() != 0) && (opStack.top() == '*' || opStack.top() == '/')) {
 				MathExpression::solveOperation(*ptNumStack, *ptOpStack);
 			}
 		}
-		// If the vector element is a parenthesis being closed:
 		else if (v1[i] == ')') {
-			// If the operator on top of the opStack is diferent of "("
-			// the operations are resolved till it reaches the "(".
 			if (opStack.top() != '(') {
 				while (opStack.top() != '(') {
 					solveOperation(*ptNumStack, *ptOpStack);
 				}
 			}
-			opStack.pop(); // It removes the operator "(" from the opStack.
-			// If the operator on top of opStack is "*" or "/" the operation is solved.
+			opStack.pop(); 
 			if ((opStack.size() != 0) && (opStack.top() == '*' || opStack.top() == '/')) {
 				solveOperation(*ptNumStack, *ptOpStack);
 			}
 		}
-		// After it solves an expression into a parenthesis, if the operator on top
-		// of opStack is "-", and the text operator is "+" or -", the operation is solved.
 		if (opStack.size() != 0 && opStack.top() == '-') {
 			if (i < v1.size() - 1 && (v1[i + 1] == '+' || v1[i + 1] == '-')) {
 				MathExpression::solveOperation(*ptNumStack, *ptOpStack);
 			}
 		}
 	}
-	// Finally the values and operators into the stacks are unstacked and
-	// the result of the expression is on top of numStack.
 	while (!opStack.empty() && numStack.size() > 1) {
 		solveOperation(*ptNumStack, *ptOpStack);
 	}
 	return(numStack.top());
 }
-
-bool MathExpression::checkLine(std::vector<char> v1) 
-{
+bool MathExpression::checkLine(std::vector<char> v1)  {
 	std::vector<char> vCheck; 
 	int checkPar = 0, errorSignal = 0;
 	try {
@@ -166,23 +137,19 @@ bool MathExpression::checkLine(std::vector<char> v1)
 			}
 			if (v1[i] == '(') { checkPar++; }
 			if (v1[i] == ')') { checkPar--; }
-			// It checks if there's a parenthesis closed without
-			// that one has opened it. Example: '1*(1))'.
+			// Parenthesis being closed without that one has been opened.
 			if (checkPar < 0) { throw 1; }
 		}
-		// It checks if all parenthesis opened were closed
-		// Example: '1+(1+(1)'
+		// If all parenthes were closed.
 		if (checkPar != 0) { throw 2; }
 		for (int i = 0; i < v1.size() - 1; i++) {
 			for (int j = 0; j < op.size(); j++) {
-				// It checks if a parenthesis being closed after
-				// an operator. Example: '(1+)1'
+				// Operator before parenthesis being closed
 				if (v1[i] == op[j] && v1[i + 1] == ')') { throw 3; }
 			}
-			// It Checks if a perenthesis being opened after
-			// a number. Example: '1(1+1)'
+			// Number before parenthesis being opened.
 			if (isdigit(v1[i]) && v1[i + 1] == '(') { throw 4; }
-			// Example: '(1+1)1
+			// Number after parenthesis being closed.
 			if (v1[i] == ')' && isdigit(v1[i + 1])) { throw 5; }
 		}
 	}
@@ -192,61 +159,50 @@ bool MathExpression::checkLine(std::vector<char> v1)
 	}
 	try {
 		for (int i = 0; i < vCheck.size(); i++) {
-			// It checks if there's characteres of the expression are valid.
-			// The input can jus contains numbers, the operators '+-*/' and '()'.
+			// Invalid operators.
 			if (!isdigit(vCheck[i])) {
 				if (vCheck[i] != '+' && vCheck[i] != '-' && vCheck[i] != '*' && vCheck[i] != '/') {
-					throw 0;
+					throw 6;
 				}
 			}
 		}
 	}
 	catch (int x) {
 		std::cout << "Error " << x << "  - Irregular charactere!" << std::endl;
-		errorSignal++;;
+		errorSignal++;
 	}
-	// It checks if there's some literal integer bigger than 9.
+	// Literal integer bigger than 9.
 	try {
 		for (int i = 0; i < v1.size() - 1; i++) {
-			if (isdigit(v1[i]) && isdigit(v1[i + 1])) { throw 6; }
+			if (isdigit(v1[i]) && isdigit(v1[i + 1])) { throw 7; }
 		}
 	}
 	catch (int x) {
-		std::cout << "Error " << x << "  - literal is too large!" << std::endl;
+		std::cout << "Error " << x << "  - Literal is too large!" << std::endl;
 		errorSignal++;
 	}	 
 	try {
-		// It checks if the first value is a negative number.
-		// Example: '((-1)+1)'
-		if (vCheck[0] == '-') { throw 7; }
+		if (vCheck[0] == '-') { throw 8; }
 		for (int i = 0; i < v1.size() - 1; i++) {
-			if (v1[i] == '(' && v1[i + 1] == '-') { throw 8; }
-		}
-		for (int i = 0; i < vCheck.size() - 1; i++) {
-			if (vCheck[i] == '*' || vCheck[i] == '/') {
-				// It checks if there's a negative number after
-				// a '*' or '/' operation. Example: '1*(-1)'
-				if (vCheck[i + 1] == '-') { throw 8; }
-			}		
+			if (v1[i] == '(' || v1[i] == '*' || v1[i] == '/') {
+				if (v1[i + 1] == '-') { throw 9; }
+			}
 		}
 	}
 	catch (int x) {
-		std::cout << "Error " << x << "  - negative literal!" << std::endl;
+		std::cout << "Error " << x << "  - Negative literal!" << std::endl;
 		errorSignal++;
 	}
 	try {
-		// It checks if in first position there's operators '*' or '/'
-		// Example: '*1+1'
-		if (vCheck[0] == '*' || vCheck[0] == '/') { throw 9; }
+		// Operators '*' or '/' in first position.
+		if (vCheck[0] == '*' || vCheck[0] == '/') { throw 10; }
 		for (int i = 0; i < op.size(); i++) {
-			// It checks if the last position contains any operator,
-			// Example: '1+1+'
-			if (vCheck[vCheck.size() - 1] == op[i]) { throw 10; }
-			// It checks consecultive operators.
+			// Any operator in last position.
+			if (vCheck[vCheck.size() - 1] == op[i]) { throw 11; }
+			// Operators '*' or '/' after any operator.
 			for (int j = 0; j < vCheck.size() - 1; j++) {
 				if (vCheck[j] == op[i]) {
-					// Any operator followed by '*' or '/'. Example: '1+(/1)'
-					if (vCheck[j + 1] == '*' || vCheck[j + 1] == '/') { throw 11; }
+					if (vCheck[j + 1] == '*' || vCheck[j + 1] == '/') { throw 12; }
 				}
 			}
 		}
@@ -255,6 +211,7 @@ bool MathExpression::checkLine(std::vector<char> v1)
 		std::cout << "Error " << x << "  - Operation makes no sense!" << std::endl;
 		errorSignal++;
 	}
+	// If at least one exception has been executed, returns false.
 	if (errorSignal > 0) { return false; } 
 	else				 { return(true); }
 }
--- test/testsEthereum.cpp
+++ test/testsEthereum.cpp
@@ -1,128 +1,140 @@
 #include <iostream>
 #include "../include/MathExpression.h"
 #include <gtest/gtest.h>
-#include "windows.h"
 
 MathExpression operation;
-// Ethereum tests
-TEST(Operation, test1) {
+// Tests sugested by Ethereum
+TEST(Ethereum_Tests, test1) {
 	std::string input = "(4 + 5 * (7 - 3)) - 2";
 	int expectResult = (4 + 5 * (7 - 3)) - 2;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste2) {
+TEST(Ethereum_Tests, Teste2) {
 	std::string input = "4+5+7/2";
 	int expectResult = 4 + 5 + 7 / 2;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste3) {
+TEST(Ethereum_Tests, Teste3) {
 	std::string input = "10+1";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste4) {
+TEST(Ethereum_Tests, Teste4) {
 	std::string input = "-10";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
 // Exception tests
-TEST(Operation, test5) {			//Error 1
+TEST(Exception_Tests, test0) {			//Error 0
+	std::string input = "1/0";
+	int expectResult = -1;
+	int softwareResult = operation.readLine(input);
+	EXPECT_EQ(expectResult, softwareResult);
+}
+TEST(Exception_Tests, test1) {			//Error 1
 	std::string input = "1*(1))";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste6) {		   // Error 2
+TEST(Exception_Tests, Teste2) {		   // Error 2
 	std::string input = "1 + (1 + (1)";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste7) {		  // Error 3
-	std::string input = "(1+)1";
+TEST(Exception_Tests, Teste3) {		  // Error 3
+	std::string input = "(1-)+1";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste8) {		  // Error 4
+TEST(Exception_Tests, Teste4) {		  // Error 4
 	std::string input = "1(1+1)";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, test9) {		  // Error 5
+TEST(Exception_Tests, test5) {		  // Error 5
 	std::string input = "(1+1)1";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste10) {		  // Error 6
+TEST(Exception_Tests, Teste6) {		  // Error 6
 	std::string input = "1+ (b)*3";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste11) {		  // Error 7
-	std::string input = "(-(1)+1)";
+TEST(Exception_Tests, Teste7) {		  // Error 7
+	std::string input = "(10+1)";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste12) {		  // Error 8	
+TEST(Exception_Tests, Teste8) {		  // Error 8	
+	std::string input = "-1*1";
+	int expectResult = -1;
+	int softwareResult = operation.readLine(input);
+	EXPECT_EQ(expectResult, softwareResult);
+}
+TEST(Exception_Tests, Teste9) {		  // Error 9	
 	std::string input = "1*(-1)";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, test13) {		  // Error 9
+TEST(Exception_Tests, test10) {		  // Error 10
 	std::string input = "*1+1";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
 
-TEST(Operation, Teste14) {		  // Error 10
+TEST(Exception_Tests, Teste11) {		  // Error 11
 	std::string input = "1+1*";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
 
-TEST(Operation, Teste15) {		  // Error 11
+TEST(Exception_Tests, Teste12) {		  // Error 12
 	std::string input = "1+(/1)";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste16) {		  // Many errors	
+TEST(Exception_Tests, Teste13) {		  // Many errors	
 	std::string input = "2+1)@(-4+)(5)+20//2";
 	int expectResult = -1;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, test17) {
+// Other tests.
+TEST(Other_Tests, test1) {
 	std::string input = "+4 +5/-+-2+ 4*(2+1)/2";
 	int expectResult = +4 + 5 /-+- 2 + 4 * (2 + 1) / 2;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste18) {
+TEST(Other_Tests, Teste2) {
 	std::string input = "(2/1*4/2)-3+(5+(4+(9-3)))/2";
 	int expectResult = (2 / 1 * 4 / 2) - 3 + (5 + (4 + (9 - 3))) / 2;
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste19) {
+TEST(Other_Tests, Teste3) {
 	std::string input = "4+8/4*9-8/2 +8*(-+-+-+-2)";
 	int expectResult = 4 + 18 - 4 + 8 * (-+-+-+-2);
 	int softwareResult = operation.readLine(input);
 	EXPECT_EQ(expectResult, softwareResult);
 }
-TEST(Operation, Teste20) {
+TEST(Other_Tests, Teste4) {
 	std::string input = "8-3/2+-+-5*8/3+1";
 	int expectResult = int(8 - 3 / 2 + -+-5 * 8 / 3 + 1);
 	int softwareResult = operation.readLine(input);
